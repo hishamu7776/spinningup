ff10007e564d0b6bb2ac6c94653d3d2b34b4e627
nphamilton <nathaniel_hamilton@outlook.com>
2022-03-21 19:49:57 -0500

    corrected bash commands

--- spinup/examples/stl-gym/train_agents.bash
+++ spinup/examples/stl-gym/train_agents.bash
@@ -1,21 +1,21 @@
 
 # train cartpole experiments
-./cartpole.py --train-all --config-path './configs/cartpole.yaml'
+python cartpole.py --train-all --config-path './configs/cartpole.yaml'
 
 # train pendulum experiments
-./inverted_pendulum.py --train-all --config-path './configs/pendulum_stabilize.yaml'
+python inverted_pendulum.py --train-all --config-path './configs/pendulum_stabilize.yaml'
 
 # train satellite docking experiments
-./docking.py --train-all --config-path './configs/docking.yaml'
+python docking.py --train-all --config-path './configs/docking.yaml'
 
 # train aircraft rejoin experiments
-./rejoin.py --train-all --config-path './configs/rejoin.yaml'
+python rejoin.py --train-all --config-path './configs/rejoin.yaml'
 
 # train lava crossing experiments
-./lava_crossing.py --train-all --config-path './configs/avoid_lava.yaml'
+python lava_crossing.py --train-all --config-path './configs/avoid_lava.yaml'
 
 # train door-key experiments
-./door_key.py --train-all --config-path './configs/cartpole.yaml'
+python door_key.py --train-all --config-path './configs/cartpole.yaml'
 
 # train robotic arm experiments
-./reacher.py --train-all --config-path './configs/reacher.yaml'
+python reacher.py --train-all --config-path './configs/reacher.yaml'
