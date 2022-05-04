d026380178bd4f514e26e9011de4a2b3da58d417
Nate Hamilton <nathaniel_hamilton@outlook.com>
2022-05-03 20:52:00 -0500

    fixed bash script

--- spinup/examples/formats2022/train_formats_agents.bash
+++ spinup/examples/formats2022/train_formats_agents.bash
@@ -1,8 +1,11 @@
 # Sparse vs Dense example
 python example_dense_v_sparse_pendulum.py --train-all --plot-all --plot-traces
 
-# Retraining example
-python example_retrain_partpole.py --train-all --plot-all --plot-traces
+# Bad specifications example
+python example_bad_specs.py --train-all --plot-all --plot-traces
 
 # Retraining example
+python example_retrain_cartpole.py --train-all --plot-all --plot-traces
+
+# Sequential tasks example
 python example_sequential_cartpole.py --train-all --plot-all --plot-traces
