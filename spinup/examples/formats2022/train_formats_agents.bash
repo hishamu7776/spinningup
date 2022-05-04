d9ce1adb54917bacabf6f7fb15517a9450af4ce6
nphamilton <nathaniel_hamilton@outlook.com>
2022-05-03 20:14:26 -0500

    fixed typo

--- spinup/examples/formats2022/train_formats_agents.bash
+++ spinup/examples/formats2022/train_formats_agents.bash
@@ -3,3 +3,6 @@ python example_dense_v_sparse_pendulum.py --train-all --plot-all --plot-traces
 
 # Retraining example
 python example_retrain_partpole.py --train-all --plot-all --plot-traces
+
+# Retraining example
+python example_sequential_cartpole.py --train-all --plot-all --plot-traces
