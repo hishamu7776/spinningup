38d26f5801de7cf9087ffff7d437597e237be2d3
hishamu7776 <uaj.hisham@gmail.com>
2023-08-18 08:08:05 -0700

    Added cartpole example for RMLGym

---
+++ spinup/examples/rml-gym/scripts/train_rml_cartpole_agents_stability.bash
@@ -0,0 +1,14 @@
+python test_rml_cartpole_stability.py --baseline --random-seed 1630
+python test_rml_cartpole_stability.py --baseline --random-seed 2241
+python test_rml_cartpole_stability.py --baseline --random-seed 2320
+#python test_rml_cartpole.py --baseline --random-seed 4930
+
+python test_rml_cartpole_stability.py --dense --random-seed 1630
+python test_rml_cartpole_stability.py --dense --random-seed 2241
+python test_rml_cartpole_stability.py --dense --random-seed 2320
+#python test_rml_cartpole.py --dense --random-seed 4930
+
+python test_rml_cartpole_stability.py --rml --random-seed 1630
+python test_rml_cartpole_stability.py --rml --random-seed 2241
+python test_rml_cartpole_stability.py --rml --random-seed 2320
+#python test_rml_cartpole.py --rml --random-seed 4930
